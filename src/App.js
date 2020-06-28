c8cbd2b269870f7db1f16c2d8b7bc04e9ec75101
zhai xin <xin.zhai@thoughtworks.com>
2020-06-28 17:01:43 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './styles.css';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="logo">
+          <img
+            src="https://www.freelogoservices.com/blog/wp-content/uploads/little_caesars_character-1.png"
+            width="100px"
+            alt={'old_man'}
+          />
+        </div>
+        <form noValidate>
+          <label for="username">
+            <span className="label-star">*</span> Username:
+            <input
+              id="username"
+              name="username"
+              placeholder="6~10 characters"
+              type="text"
+              noValidate
+            />
+          </label>
+          <label for="password">
+            <span className="label-star">*</span> Password:
+            <input
+              id="password"
+              name="password"
+              placeholder="6~10 numbers"
+              type="password"
+              noValidate
+            />
+          </label>
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    );
+  }
+}
